945c2662bb66c6e47d8f9966a70962931eda7f61
smurfsakun199-raul <smurfsakun199@gmail.com>
2026-04-24 18:33:31 +0700

    lawak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,26 +161,26 @@ export default function AlahBacot() {
             <HeaderTolol />
             <main>
                 <section aria-labelledby="bacot-title" className="pad-bl-20px max-w-1210px margin-auto pad-top-48px pad-0-14px">
-                    <div className="glow tolol-purple"></div>
+                    <div className="glow tolol-blue-old"></div>
                     <div className="flex-mx-1024px-dir-clm jus-c-c align-itm-inherit gap-24px">
-                        <div>
+                        <div className="pos-rel">
                             <div className="flex jus-c-l gap-10px pad-btm-10px">
                                 <Palette />
                                 <span>Experinece Style</span>
                             </div>
                             <div className="pad-btm-40px">
-                                <div className="glow tolol-pink"></div>
+                                <div className="glow tolol-blue"></div>
                                 <h1 id="bacot-title" className="font-size-40px pad-btm-20px">Design Portfolio dengan memadukan <span className="glow-text">Ketololan</span> yang <span className="glow-text">Unik, Rapi</span> dan <span className="glow-text">Elegan</span></h1>
                                 <p>Deskripsi ini mungkin membuat anda akan menyesal jika sampai membacanya sampai selesai,dikarenakan kata yang saya gunakan merepetisi sebuah kata menjadi berulang ulang,dan akhirnya yang anda dapat hanyalah tautologi dan tak menguntungkan anda.</p>
                             </div>
                             <div className="flex pad-btm-20px">
-                                <div className="pad-8-18px br-op br-radius-12px">
+                                <div className="pad-4-18px br-op br-radius-12px bg-blur-card">
                                     <h2 className="font-size-16px flex align-itm-c gap-10px"><CodeXml /> Code</h2>
                                 </div>
                             </div>
                             <div className="grid grid-temp-clm-r3 jus-c-c align-itm-inherit gap-20px">
                                 {listYear.map((item) => (
-                                    <div key={item.id} className="card-hover br-radius-12px">
+                                    <div key={item.id} className="card-hover bg-blur-card br-radius-12px">
                                         <div className="flex align-itm-c pad-btm-10px gap-10px">
                                             {item.title}
                                             <span className="font-size-12px color-p">{item.name}</span>
@@ -195,9 +195,10 @@ export default function AlahBacot() {
                                     </div>
                                 ))}
                             </div>
+                            <div className="glow tolol-noname"></div>
                         </div>
 
-                        <div className="card-hover-dig-to-left width-100ps">
+                        <div className="card-hover-dig-to-left bg-blur-card width-100ps">
                             <div className="">
                                 <div className="flex jus-c-sb align-itm-c gap-10px pad-btm-10px">
                                     <div className="icn-circle-svg flex gap-10px">
@@ -243,19 +244,19 @@ export default function AlahBacot() {
 
                 <section aria-labelledby="road-bacot" className="pad-bl-20px max-w-1210px margin-auto pad-top-48px pad-0-14px">
                     <div className="flex pad-btm-20px">
-                        <div className="pad-8-18px br-op br-radius-12px">
+                        <div className="pad-4-18px br-op br-radius-12px bg-blur-card">
                             <h2 id="road-bacot" className="font-size-16px flex jus-c-c align-itm-c gap-10px"><Compass />Road</h2>
                         </div>
                     </div>
                     <div className="flex-mx-1024px-dir-clm jus-c-c align-itm-inherit gap-24px">
-                        <div className="card">
+                        <div className="card bg-blur-card">
                             <h3 className="font-size-14px color-p pad-btm-20px">Portfolio</h3>
                             <h4 className="font-size-24px pad-btm-30px">Portfolio yang didesain agar anda bosan membaca setiap kalimatnya.</h4>
                             <p>Memvisualisasikan penempatan posisi yang adaptif serta fleksibel. Dengan mengimplementasikan cara ini,akan membuat anda menjadi bosan dengan harapan anda pada kami.</p>
                         </div>
                         <div className="flex-mx-764px-dir-clm gap-20px">
                             {roadLol.map((item) => (
-                                <div key={item.id} className="card">
+                                <div key={item.id} className="card bg-blur-card">
                                     <div className="flex jus-c-sb align-itm-c pad-btm-10px gap-10px">
                                         <h3 className="font-size-14px color-p">{item.years}</h3>
                                         <span className="font-size-14px color-p">{item.month}</span>
@@ -268,9 +269,10 @@ export default function AlahBacot() {
                     </div>
                 </section>
 
-                <section aria-labelledby="skill-bacot" className="pad-bl-20px max-w-1210px margin-auto pad-top-48px pad-0-14px">
+                <section aria-labelledby="skill-bacot" className="pos-rel pad-bl-20px max-w-1210px margin-auto pad-top-48px pad-0-14px">
+                    <div className="glow tolol-blue-old"></div>
                     <div className="flex pad-btm-20px">
-                        <div className="pad-8-18px br-op br-radius-12px">
+                        <div className="pad-4-18px br-op br-radius-12px bg-blur-card">
                             <h2 id="skill-bacot" className="font-size-16px flex jus-c-c align-itm-c gap-10px"><LayersPlus />Skills</h2>
                         </div>
                     </div>
@@ -287,7 +289,7 @@ export default function AlahBacot() {
                         </div>
                         <div className="grid grid-temp-clm-r4 jus-c-c gap-20px">
                             {skillsBacot.map((item) => (
-                                <div key={item.id} className="card-hover">
+                                <div key={item.id} className="card-hover bg-blur-card">
                                     <div className="flex jus-c-sb gap-20px pad-btm-20px">
                                         <h2 className="font-size-16px">{item.skill}</h2>
                                         <strong className="flex jus-c-c align-itm-c gap-10px font-size-14px"><Star className="width-18px height-18px" />{item.skr}</strong>
